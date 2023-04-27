779a9c3066f08058fbf69393a9e400762e848794
PhatPhacius <yusuki2712@gmail.com>
2023-04-27 23:56:11 +0700

    add  bean for secure service

---
+++ secure/src/main/java/com/microservice/secure/config/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.microservice.secure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        return null;
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return null;
+    }
+
+
+
+}
